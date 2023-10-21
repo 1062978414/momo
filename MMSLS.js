0fd96821675f67ec7c1d97209cc3d4c258df94ba
1062978414 <43368834+1062978414@users.noreply.github.com>
2023-10-21 23:05:22 +0800

    更新 MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -1,4 +1,4 @@
-const MMSLS = ["https://www.maimemo.com/share/page?uid=28382088&pid=99d1c2b656596efa4308a3f28eb2ff61&tid=16b0bdead3a8940727aba2bbd6e54cb2",]
+const MMSLS = ["https://www.maimemo.com/share/page?uid=28382088&pid=6e98273e7bd3f017cff7291815c0b71e&tid=80207c1c018b63a5a1f84a4dc9d5018d",]
   /**
    * 生成随机数字
    * @param {number} min 最小值（包含）
